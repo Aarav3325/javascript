6d1695ff0f6585d5572d9b5d66fb789a7a5764f9
Aarav3325 <68527036+Aarav3325@users.noreply.github.com>
2023-09-17 21:32:38 +0530

    Day 3

--- day2.js
+++ day2.js
@@ -1,14 +1,10 @@
-/**
- * @param {number} n
- * @return {Function} counter
- */
-var createCounter = function(n) {
-    return function() {
+var createCounter = function (n) {
+    return function () {
         return n++;
     };
 };
 
- 
+
 const counter = createCounter(10)
 console.log(counter());
 console.log(counter());
