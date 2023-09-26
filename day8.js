08d2eb602dd41da3cd3820de04e211f6f21e1659
Aarav3325 <68527036+Aarav3325@users.noreply.github.com>
2023-09-26 19:35:20 +0530

    day 9

--- day8.js
+++ day8.js
@@ -1,5 +1,5 @@
-var compose = function(functions) {
-    return function(x) {
+var compose = function (functions) {
+    return function (x) {
         let result = x;
         for (let i = functions.length - 1; i >= 0; i--) {
             result = functions[i](result);
